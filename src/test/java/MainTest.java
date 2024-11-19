9860bf22e568e5c49bbb31b631f2c53d8e3d0e00
elfarsif <f.n.elfarsi@gmail.com>
2024-11-19 12:43:54 -0500

    Initial commit : open game window and added basic screen settings

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+
+import org.frank.Main;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+
+    }
+}
