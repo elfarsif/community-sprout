e4a336bfb5146fd448ffd4feabfed580d921601e
elfarsif <f.n.elfarsi@gmail.com>
2024-11-19 12:51:58 -0500

    added game thread

--- src/main/java/org/frank/GamePanel.java
+++ src/main/java/org/frank/GamePanel.java
@@ -3,19 +3,19 @@ package org.frank;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 
     //SCREEN SETTINGS
     final int originalTileSize = 16;
     final int scale = 3;
 
     final int tileSize = originalTileSize * scale;
-
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
+
+    Thread gameThread;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -23,4 +23,13 @@ public class GamePanel extends JPanel {
         this.setDoubleBuffered(true);
     }
 
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
